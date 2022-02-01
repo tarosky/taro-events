d6cc658495683cc57e04e725236458198f6d17b2
Ko Takagi <knockoutmarch@gmail.com>
2022-02-02 02:16:47 +0900

    Update version

--- taro-events.php
+++ taro-events.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/tarosky/taro-events
  * Description: Add events feature to your WordPress site.
  * Author: Tarosky INC.
- * Version: 1.0.3
+ * Version: 1.0.5
  * Author URI: https://tarosky.co.jp/
  * License: GPL3 or later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
